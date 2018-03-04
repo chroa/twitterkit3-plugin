334a1f8acbb60e7659552647e6598ff268ff2926
Joana <joana.rocha@tecnico.ulisboa.pt>
2018-03-04 12:30:15 +0000

    Removed unused functions.

--- www/TwitterConnect.js
+++ www/TwitterConnect.js
@@ -14,15 +14,6 @@ var TwitterConnect = {
 		exec(successCallback, errorCallback, 'TwitterConnect', 'verifyCredentials', [{"include_entities" : args.include_entities,
 																					"skip_status" : args.skip_status,
 																					"include_email" : args.include_email}]);
-	},
-	sendTweet: function (msg, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'TwitterConnect', 'sendTweet', [{"status" : msg}]);
-	},
-	openComposer: function (text, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'TwitterConnect', 'openComposer', [text]);
-	},
-	showTimeline: function (query, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'TwitterConnect', 'showTimeline', [query]);
 	}
 };
 
